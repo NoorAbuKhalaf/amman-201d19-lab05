8cb4344e0c5221b4738d0d6bc9f3eb8b9399f4f4
NoorAbuKhalaf <noor.abukhalaf@hotmail.com>
2021-03-18 17:01:41 +0200

    solve problem3

--- app.js
+++ app.js
@@ -51,11 +51,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    let level1= sum(a,b)[0]+c;
+    let level2= multiply(a,b)[0]*c;
+    return[level1,level2,a+' and '+b+' and '+c+ ' sum to '+level1+'.','The product of '+a+' and '+b+' and '+c+ ' is '+level2+'.']
 }
+// console.log(sumAndMultiply(4,7,5));
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
